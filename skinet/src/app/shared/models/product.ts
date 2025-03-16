6126cd67ecafc2545005d477ef91131545db20c1
hendidwipurwanto <hendi.purwanto@gustodian.com>
2025-03-16 16:27:11 +0700

    using types angular in our project

---
+++ skinet/src/app/shared/models/product.ts
@@ -0,0 +1,10 @@
+export type Product ={ /** ini si neil cumming prefer ini tapi gw hendi prefer interface cmn belom coba aja sama kata chat gpt prefer interface fungsinya mirip dto dan model di asp.net */
+    id:number;
+    name:string;
+    description:string;
+    price:number;
+    pictureUrl:string;
+    type:string;
+    brand:string;
+    quantityInStock:number;
+}
