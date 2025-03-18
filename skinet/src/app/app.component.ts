1f1300af49b2e7b424c6048a510605528d9b24f0
hendidwipurwanto <hendi.purwanto@gustodian.com>
2025-03-18 11:53:53 +0700

    Designing the shop page

--- skinet/src/app/app.component.ts
+++ skinet/src/app/app.component.ts
@@ -5,25 +5,16 @@ import { HeaderComponent } from "./layout/header/header.component";
 import { HttpClient } from '@angular/common/http';
 import { Pagination } from './shared/models/pagination';
 import { ShopService } from './core/services/shop.service';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
-private shopService = inject(ShopService);
-  title = 'skinet';
-  products: Product[] =[];
-
-  ngOnInit(): void {
-    this.shopService.getProducts().subscribe({
-      next: response=> this.products = response.data,
-      error: error => console.log(error),
-      complete: () => console.log('complete')      
-    })
-  }
+export class AppComponent {
+ title='Skinet';
   
 }
